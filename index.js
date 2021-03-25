2e13febfbde549a9887bc6337f1707c607169dcd
oddcptinkirk22 <37202566+oddcptinkirk22@users.noreply.github.com>
2021-03-25 03:01:17 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
-const {Client, Richembed} = require(`discord.js`);
+const randomPuppy = require(`random-puppy`)
+const {Client, RichEmbed} = require(`discord.js`);
 const config = require('./config.json');
 const fs = require("fs");
 const prefix = `,`
 
+
+
 console.log('online!');
 
+
+
 bot.commands = new Discord.Collection();
+
+
 
 bot.on(`guildMemberAdd`, member =>{
 
@@ -16,6 +23,8 @@ bot.on(`guildMemberAdd`, member =>{
 
   channel.send(`welcome to CaveCreaft, ${member}, Read rules or do ,serverinfo for a list of rules and the server IP`)
 });
+
+
 
 ("commands")
 fs.readdir("./commands/", (err, files) =>{
@@ -35,6 +44,100 @@ jsfile.forEach((f, i) =>{
   bot.commands.set(props.config.name, props);
 
 });
+
+
+
+
+("turning on and stuff")
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online! Its on ${bot.guilds.size} severs!`);
+    bot.user.setActivity("working on it", {type: "working on it"});
+});
+
+
+
+bot.on("guildMemberAdd", async member => {
+  let welcomchannel = member.guild.channels.find('name', "sup");
+  if(!welcomchannel) return;
+  welcomchannel.send('welcome ${member} to the sever!');
+  member.send("test").catch(O_o=>{ welcomchannel.send("dis kid dont got dms")});
+});
+
+
+
+bot.on("guildMemberremove", async member => {
+  let welcomchannel = member.guild.channels.find('name', "sup");
+  if(!welcomchannel) return;
+  welcomchannel.send('welcome ${member} to the sever!');
+  member.send("test").catch(O_o=>{ welcomchannel.send("dis kid dont got dms")});
+});
+
+
+
+
+("sever info/user/bot")
+bot.on("message", async message =>{
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+let prefix = config.prefix;
+let msgarray = message.content.split(" "); //>test hi
+let command = msgarray[0];
+if(command.slice(0, prefix.length) !== prefix) return;
+let args = msgarray.slice(1);
+let commandfile = bot.commands.get(command.slice(prefix.length))
+if(commandfile) commandfile.run(bot, message, args);
+if(command === `${prefix}botinfo`){
+
+  let bicon = bot.user.displayAvatarURL;
+  let botembed = new Discord.MessageEmbed()
+  .setDescription("Bot Information")
+  .setColor("#15f153")
+  .setThumbnail(bicon)
+  .addField("Bot name", bot.user.username)
+  .addField("Made on", bot.user.createdAt);
+
+  return message.channel.send(botembed);
+};
+
+
+if(command === `${prefix}serverhelp`){
+  const embed = new Discord.MessageEmbed()
+  .setColor(`#FF0000`)
+  .setTitle(`Thank you for useing CaveCraft, if you need help with anything please use the #support channel or go to our website "url here"`)
+  message.author.send(embed);
+}
+
+
+
+
+
+
+if(command === `${prefix}purge`)
+{
+    purge();
+    async function purge(){
+       message.delete(); // deletes the command message
+
+       const args = message.content.slice(prefix.length).trim().split(' ');
+       const command = args.shift().toLowerCase();
+
+       if(!message.member.roles.cache.some(r => r.name === "Purge")) {
+           message.channel.send('You Cannot use this command');
+           return;
+       }
+
+       if(isNaN(args[0])){
+           // Sends a message to the channel
+           message.channel.send('Please use a number as your arguments');
+           // returns so the rest of the code doesn't run
+           return;
+       }
+
+       message.channel.bulkDelete(args[0])
+       .catch(error => message.channel.send(Error, `${error}`));
+}
+}
+})
 
 
 
